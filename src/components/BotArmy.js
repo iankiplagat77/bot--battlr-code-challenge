dee6f4f1962f4ceea6d40de275efe31e521c5e7f
ian <ianokipla@gmail.com>
2023-11-20 20:36:14 +0300

    added files

---
+++ src/components/BotArmy.js
@@ -0,0 +1,20 @@
+import React from "react";
+import BotCard from "../components/BotCard";
+
+const BotArmy = props => {
+  //bot army code here...
+  return (
+    <div className="ui segment inverted olive bot-army">
+      <div className="ui five column grid">
+        <div className="row bot-army-row">
+          {props.myBots.map(bot => {
+            return <BotCard key={bot.id} bot={bot} />
+          })}
+           Bot Army
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BotArmy;
